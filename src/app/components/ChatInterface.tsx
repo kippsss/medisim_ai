5df9a6fa23a4d03cefb484c02fd49b2ea95b9633
kippsss <keithchewzk@gmail.com>
2024-10-30 12:58:32 +0800

    add chat bubbles

--- src/app/components/ChatInterface.tsx
+++ src/app/components/ChatInterface.tsx
@@ -1,9 +1,38 @@
+"use client";
+import { useState } from "react";
+
 export default function ChatInterface() {
+  const [messages, setMessages] = useState([
+    { role: "user", text: "Hello there!" },
+    { role: "assistant", text: "General Kenobi!" },
+  ]);
+
   return (
-    <input
-      type="text"
-      placeholder="Type here"
-      className="input w-full max-w-xs"
-    />
+    <div className="flex flex-col">
+      {/* Chat Messages */}
+      {messages.map((message, index) => (
+        <div
+          key={index}
+          className={`chat ${
+            message.role === "assistant" ? "chat-start" : "chat-end"
+          } mb-5`}
+        >
+          <div
+            className={`chat-bubble ${
+              message.role === "assistant" ? "" : "chat-bubble-primary"
+            }`}
+          >
+            {message.text}
+          </div>
+        </div>
+      ))}
+
+      {/* Chat Input */}
+      <input
+        type="text"
+        placeholder="Type here"
+        className="input w-full max-w-xs"
+      />
+    </div>
   );
 }
