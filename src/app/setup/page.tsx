1155dac02c0dde0dc25f08e4918c6cdb02253db5
kippsss <keithchewzk@gmail.com>
2024-11-05 00:17:58 +0800

    get all possible diagnoses as env var

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -8,14 +8,15 @@ export default function Setup() {
     router.push('/chat');
   };
 
-  const diagnoses = ['Common cold', 'Cough', 'Diabetes', 'Asthma'];
+  const diagnosesString = process.env.NEXT_PUBLIC_DIAGNOSES || '';
+  const diagnoses = diagnosesString.split(',');
 
   return (
     <div className="flex flex-col p-40 h-screen justify-center">
       <h1 className="text-5xl font-bold">{TITLE}</h1>
       <div className="flex flex-row mt-20">
         <div className="flex flex-row justify-center w-1/2 form-control">
-          <ul className="menu bg-base-200 rounded-box w-full">
+          <ul className="menu bg-base-200 rounded-box w-full max-h-64 overflow-y-auto flex-nowrap">
             {diagnoses.map((diagnosis, index) => (
               <li key={index}>
                 <label className="label cursor-pointer">
