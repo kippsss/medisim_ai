596fe4cc8023446f4b5f359a1b6b4a5cf4bc5972
kippsss <keithchewzk@gmail.com>
2024-11-07 14:37:07 +0800

    conditioanl render when diagnosis correct

--- src/app/chat/components/ChatDiagnoseModal.tsx
+++ src/app/chat/components/ChatDiagnoseModal.tsx
@@ -1,27 +1,32 @@
 'use client';
+import { useState } from 'react';
 import { useDiagnoses } from '../../contexts/DiagnosesContext';
+
+import { DIAGNOSE_SELECTION_TITLE, DIAGNOSE_CORRECT_TEXT } from '../constants';
 
 interface Props {
   actualDiagnosis: string;
 }
 
-const openDiagnoseModal = () => {
-  const modal = document.getElementById('diagnose-modal');
-  if (modal) {
-    (modal as HTMLDialogElement).showModal();
-  }
-};
-
-const checkAnswer = (selectedDiagnosis: string, actualDiagnosis: string) => {
-  if (selectedDiagnosis == actualDiagnosis) {
-    console.log('Diagnosis correct');
-  } else {
-    console.log('Diagnosis incorrect');
-  }
-};
-
 export default function ChatDiagnoseModal({ actualDiagnosis }: Props) {
+  const [correct, setCorrect] = useState(false);
+
   const { diagnoses } = useDiagnoses();
+
+  const openDiagnoseModal = () => {
+    const modal = document.getElementById('diagnose-modal');
+    if (modal) {
+      (modal as HTMLDialogElement).showModal();
+    }
+  };
+
+  const checkAnswer = (selectedDiagnosis: string, actualDiagnosis: string) => {
+    if (selectedDiagnosis == actualDiagnosis) {
+      setCorrect(true);
+    } else {
+      setCorrect(false);
+    }
+  };
 
   console.log(actualDiagnosis);
 
@@ -32,25 +37,45 @@ export default function ChatDiagnoseModal({ actualDiagnosis }: Props) {
       </button>
       <dialog id="diagnose-modal" className="modal">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Diagnose me</h3>
-          <ul className="mt-4 menu bg-base-200 rounded-box w-full max-h-96 overflow-y-auto flex-nowrap">
-            {Object.keys(diagnoses).map((diagnosis, index) => (
-              <li
-                key={index}
-                onClick={() => {
-                  checkAnswer(diagnosis, actualDiagnosis);
-                }}
-              >
-                <label className="label cursor-pointer">
-                  <span className="label-text">{diagnosis}</span>
-                </label>
-              </li>
-            ))}
-          </ul>
+          {/* MODAL HEADER */}
+          {!correct && (
+            <h3 className="font-bold text-lg">{DIAGNOSE_SELECTION_TEXT}</h3>
+          )}
+
+          {/* MODAL BODY */}
+          {!correct ? (
+            <ul className="mt-4 menu bg-base-200 rounded-box w-full max-h-96 overflow-y-auto flex-nowrap">
+              {Object.keys(diagnoses).map((diagnosis, index) => (
+                <li
+                  key={index}
+                  onClick={() => {
+                    checkAnswer(diagnosis, actualDiagnosis);
+                  }}
+                >
+                  <label className="label cursor-pointer">
+                    <span className="label-text">{diagnosis}</span>
+                  </label>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="flex flex-col gap-8">
+              <h3 className="text-2xl">{DIAGNOSE_CORRECT_TEXT}</h3>
+              <h1 className="font-bold text-5xl text-center">
+                {actualDiagnosis}
+              </h1>
+            </div>
+          )}
+
+          {/* MODAL FOOTER */}
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
+              {!correct ? (
+                <button className="btn">Close</button>
+              ) : (
+                <button className="btn">Next scenario</button>
+              )}
             </form>
           </div>
         </div>
