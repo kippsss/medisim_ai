6646def583cf3dd86ec0dbb8524bd7338d1bcb46
kippsss <keithchewzk@gmail.com>
2024-11-07 14:36:53 +0800

    conditinal rendering when diagnosis is correct

--- src/app/chat/constants.ts
+++ src/app/chat/constants.ts
@@ -41,3 +41,7 @@ export const TITLE = 'Welcome to medisimAI';
 export const SUBTITLE = `Your virtual patient is here for treatment — can you diagnose me? Ask questions, assess symptoms, and respond like a doctor. When you’re ready, diagnose me. Let’s see if you get it right!`;
 
 export const READY_BUTTON_TEXT = "I'm ready";
+
+export const DIAGNOSE_SELECTION_TITLE = 'Diagnose me';
+
+export const DIAGNOSE_CORRECT_TEXT = "That's right! I have";
