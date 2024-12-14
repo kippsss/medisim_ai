219e60cf77d9098075f428b897c7d0b8ff47ffca
kippsss <keithchewzk@gmail.com>
2024-12-14 20:59:19 +0800

    mic button

--- src/app/chat/components/ChatInput.tsx
+++ src/app/chat/components/ChatInput.tsx
@@ -24,6 +24,7 @@ export default function ChatInput({
           value={input}
           onChange={handleInputChange}
         />
+        {/* Submit Button */}
         <button
           className={`btn btn-circle w-9 h-9 min-h-2 ${
             (disabled || input === '') && 'btn-disabled'
