10fcb6f03ada54aa4620c8e2d9ef719b1ef65283
kippsss <keithchewzk@gmail.com>
2024-12-20 18:28:47 +0800

    set relevant timers for recording

--- src/app/chat/components/ChatAudio.tsx
+++ src/app/chat/components/ChatAudio.tsx
@@ -2,22 +2,19 @@
 import { useRef, useState } from 'react';
 import Image from 'next/image';
 interface Props {
-  recording: boolean;
   toggleRecording: () => void;
   recordingElapsedTime: number;
 }
 
 export default function ChatAudio({
-  recording,
   toggleRecording,
   recordingElapsedTime,
 }: Props) {
-  const [progress, setProgress] = useState(0);
-  const audioRef = useRef<HTMLAudioElement>(null);
-
   return (
     <div
-      className="radial-progress after:content-none"
+      className={`radial-progress after:content-none ${
+        recordingElapsedTime === 0 ? 'before:content-none' : ''
+      }`}
       style={
         {
           '--value': recordingElapsedTime * 5,
@@ -28,11 +25,15 @@ export default function ChatAudio({
       role="progressbar"
     >
       <button
-        className={`btn btn-circle w-9 h-9 min-h-2 z-10`}
+        className="btn btn-circle w-9 h-9 min-h-2 z-10"
         onClick={toggleRecording}
       >
         <Image
-          src={!recording ? 'icons/microphone.svg' : 'icons/stopRounded.svg'}
+          src={
+            recordingElapsedTime === 0
+              ? 'icons/microphone.svg'
+              : 'icons/stopRounded.svg'
+          }
           alt={'Record'}
           width={28}
           height={28}
